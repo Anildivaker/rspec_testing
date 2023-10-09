d5d4e48a979607d9f93a315b284f464fa4254f51
anil divaker <anildivaker87@gmail.com>
2023-10-09 11:20:31 +0530

    add password validtion and modify previous RSpec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,19 @@
 class User < ApplicationRecord
-    # has_secure_password
+    has_secure_password
     validates :name, presence: true, length: { minimum: 2 }
     validates :surname, presence: true, length: { maximum: 30 }
     validates :username, presence: true, length: { maximum: 30 }, uniqueness: true
     validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, presence: true, uniqueness: true
     validates :number, presence: true, uniqueness: true, numericality: true, length: { :minimum => 10, :maximum => 15 }
-    # PASSWORD_REQUIREMENT = /\A
-    #     (?=.{8,}) #minimum 8 char. long
-    #     (?=.*\d) #contain at least one num
-    #     (?=.*[a-z]) #minimum 1 lowercase letter
-    #     (?=.*[A-Z]) #minimum 1 uppercase letter
-    #     (?=.*[[:^alnm:]]) #at least one symbol
-    # /x
-    # validates :password, format: PASSWORD_REQUIREMENT
 
+    PASSWORD_FORMAT = /\A
+        (?=.{8,})          # Must contain 8 or more characters
+        (?=.*\d)           # Must contain a digit
+        (?=.*[a-z])        # Must contain a lower case character
+        (?=.*[A-Z])        # Must contain an upper case character
+        (?=.*[[:^alnum:]]) # Must contain a symbol
+    /x
+
+    validates :password, 
+    format: { with: PASSWORD_FORMAT }
 end
