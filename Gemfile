1d3909fd8648aaca0fd6a913b6afd0368e5e8ca2
anil divaker <anildivaker87@gmail.com>
2023-11-01 17:35:06 +0530

    add admin on api base project

--- Gemfile
+++ Gemfile
@@ -24,9 +24,11 @@ gem "puma", "~> 5.0"
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 gem "bcrypt", "~> 3.1.7"
 gem 'jwt'
-gem "devise"
+gem 'devise'
 gem 'activeadmin'
 gem 'sass-rails'
+gem 'sassc-rails'
+gem 'sprockets'
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+    # include AbstractController::Layouts
+    
+    include ActionView::Layouts
+  protect_from_forgery unless: -> { request.format.json? }
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
     def encode_token(payload)
         JWT.encode(payload, 'secret')
     end
@@ -26,4 +32,10 @@ class ApplicationController < ActionController::API
     def authorize
         render json: { message: 'You have to log in.' }, status: :unauthorized unless authorized_user
     end
+    protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
+  end
+    
 end
